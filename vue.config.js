94011843d3ab43000748da851a9c9d36804b4623
Bjoern Schilberg <bjoern@intevation.de>
2019-11-08 12:16:46 +0100

    Disabled productionSourceMap builds

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath:
-    process.env.NODE_ENV === "production" ? "./" : "/",
-    transpileDependencies: ['vuetify']
+  publicPath: process.env.NODE_ENV === "production" ? "./" : "/",
+  transpileDependencies: ["vuetify"],
+  productionSourceMap: false
 };
